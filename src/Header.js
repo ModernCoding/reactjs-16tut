6f02b717c7db7eb6ad4640a04ee4d040c7089217
Cormorant Programming <cormorant.programming@gmail.com>
2022-04-12 18:23:26 +0700

    useWindowSize

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,33 @@
-const Header = ({ title }) => {
-    return (
-        <header className="Header">
-            <h1>{title}</h1>
-        </header>
-    )
+import { FaLaptop, FaTabletAlt, FaMobileAlt } from 'react-icons/fa'
+
+const icon = width => {
+
+  const components
+    = [ (<FaMobileAlt />), (<FaTabletAlt />), (<FaLaptop />) ]
+  
+  const widths = [ 768, 992 ]
+
+  return (function getComponent (i) {
+
+    if (i >= widths.length) { return components [ i ] }
+
+    return width < widths [ i ] ? components [ i ] : getComponent (++i)
+
+  }) (0)
+
 }
+
+
+const Header = ({ title, width }) => {
+
+  return (
+    <header className="Header">
+      <h1>{title}</h1>
+      {icon (width)}
+    </header>
+  )
+
+}
+
 
 export default Header
