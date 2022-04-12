116cc78126c1db6f487a38a4dd97de74cf3e6f3d
Cormorant Programming <cormorant.programming@gmail.com>
2022-04-12 19:28:08 +0700

    useAxiosFetch

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { useState, useEffect } from 'react'
 import { format } from 'date-fns'
 import api from './api/posts'
 import useWindowSize from './hooks/useWindowSize'
+import useAxiosFetch from './hooks/useAxiosFetch'
 
 function App() {
   const [posts, setPosts] = useState([])
@@ -20,27 +21,37 @@ function App() {
   const [editTitle, setEditTitle] = useState('');
   const [editBody, setEditBody] = useState('');
   const navigate = useNavigate()
+
   const { width } = useWindowSize ()
+  
+  const { data, fetchError, isLoading }
+    = useAxiosFetch ('http://localhost:3500/posts')
+
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await api.get('/posts');
-        setPosts(response.data);
-      } catch (err) {
-        if (err.response) {
-          // Not in the 200 response range 
-          console.log(err.response.data);
-          console.log(err.response.status);
-          console.log(err.response.headers);
-        } else {
-          console.log(`Error: ${err.message}`);
-        }
-      }
-    }
+    setPosts(data)
+  }, [data])
 
-    fetchPosts();
-  }, [])
+
+  // useEffect(() => {
+  //   const fetchPosts = async () => {
+  //     try {
+  //       const response = await api.get('/posts');
+  //       setPosts(response.data);
+  //     } catch (err) {
+  //       if (err.response) {
+  //         // Not in the 200 response range 
+  //         console.log(err.response.data);
+  //         console.log(err.response.status);
+  //         console.log(err.response.headers);
+  //       } else {
+  //         console.log(`Error: ${err.message}`);
+  //       }
+  //     }
+  //   }
+
+  //   fetchPosts();
+  // }, [])
   
 
   useEffect(() => {
@@ -105,7 +116,13 @@ function App() {
         setSearch={setSearch}
         width={width}
       />}>
-        <Route index element={<Home posts={searchResults} />} />
+
+        <Route index element={<Home
+          posts={searchResults} 
+          fetchError={fetchError}
+          isLoading={isLoading}
+        />} />
+
         <Route path="post">
           <Route index element={<NewPost
             handleSubmit={handleSubmit}
